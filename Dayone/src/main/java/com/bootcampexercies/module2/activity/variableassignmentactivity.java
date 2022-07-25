c58ae7ff5910df088f6bc2a978b147f21e54f938
ahmed <ahmed2050.mahmoud@gmail.com>
2022-07-25 13:50:17 +0200

    variableassignmentactivity

---
+++ Dayone/src/main/java/com/bootcampexercies/module2/activity/variableassignmentactivity.java
@@ -0,0 +1,9 @@
+package com.bootcampexercies.module2.activity;
+
+public class variableassignmentactivity {
+    public static void main(String[] args) {
+        int A =10;
+        A =20+20;
+        System.out.println(A);
+    }
+}
