687f94ba5cc59c246ae4d8c6681c3ad8da357185
ahmed <ahmed2050.mahmoud@gmail.com>
2022-07-25 16:12:14 +0200

    largernumber

---
+++ Dayone/src/main/java/com/bootcampexercies/module2/activity/largernumber.java
@@ -0,0 +1,15 @@
+package com.bootcampexercies.module2.activity;
+
+public class largernumber {
+    public static void main(String[]args){
+        int num1=100,num2=50;
+        if(num1==num2)
+            System.out.println("both are equal ");
+        else if(num1>num2)
+            System.out.println(num1+"is greater ");
+        else
+            System.out.println(num2+" is greater ");
+
+    }
+
+}
