3cadeeb3c5a6f028dd3f2dc35b8c8df598733034
ahmed <ahmed2050.mahmoud@gmail.com>
2022-07-25 13:09:32 +0200

    First Day java

---
+++ Dayone/src/main/java/dayone.java
@@ -0,0 +1,11 @@
+public class dayone {
+    public static void main(String[] args) {
+        int i,j;
+        i=5;
+        j=7;
+        int sum=i+j;
+        System.out.println("The sum is "+i+j);
+        System.out.println("The sum is "+(i+j));
+        System.out.println("The sum is "+sum);
+    }
+}
