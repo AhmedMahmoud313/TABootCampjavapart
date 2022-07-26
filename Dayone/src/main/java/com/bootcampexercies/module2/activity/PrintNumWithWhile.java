bdc4f1bb9c7171e625f179adf686047114d63bc6
ahmed <ahmed2050.mahmoud@gmail.com>
2022-07-26 08:59:41 +0200

    PrintNumWithWhile

---
+++ Dayone/src/main/java/com/bootcampexercies/module2/activity/PrintNumWithWhile.java
@@ -0,0 +1,13 @@
+package com.bootcampexercies.module2.activity;
+
+public class PrintNumWithWhile {
+    public static void main(String args[]) {
+        int num = 100;
+        System.out.print("Odd Numbers from 1 to "+num+" are: ");
+        for (int i = 1; i <= num; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
