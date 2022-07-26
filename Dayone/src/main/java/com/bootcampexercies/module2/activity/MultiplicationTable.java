f09ff9574060d2790c5d61a4e96f5b0e61494bc1
ahmed <ahmed2050.mahmoud@gmail.com>
2022-07-26 14:44:43 +0200

    MultiplicationTable

--- Dayone/src/main/java/com/bootcampexercies/module2/activity/MultiplicationTable.java
+++ Dayone/src/main/java/com/bootcampexercies/module2/activity/MultiplicationTable.java
@@ -2,9 +2,10 @@ package com.bootcampexercies.module2.activity;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
-        for (int y = 11; y < 20; ++y) {
-            for (int x = 11; x < 20; ++x) {
-                System.out.printf("%4d", y*x);
+        for (int y = 11; y < 20; y++) {
+            System.out.println("-- Table  "+y + " --");
+            for (int x = 11; x < 20; x++) {
+                System.out.printf(y+"*"+x+"="+y*x);
             }
             System.out.println();
         }
