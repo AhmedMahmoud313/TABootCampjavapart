919bfcc4ca915337af79c4c5054b782e367b069d
ahmed <ahmed2050.mahmoud@gmail.com>
2022-07-28 08:54:34 +0200

    TryCatchFinallyActivity

--- Dayone/src/main/java/com/bootcampexercies/module7/activity/TryCatchFinallyActivity.java
+++ Dayone/src/main/java/com/bootcampexercies/module7/activity/TryCatchFinallyActivity.java
@@ -9,7 +9,7 @@ public class TryCatchFinallyActivity {
     void catchMeIfYouCan() {
         int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-        //TODO: use try
+
 
         try {
             for (int i = 0; i <= 10; i++) {
